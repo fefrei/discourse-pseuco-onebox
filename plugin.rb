efcd9ea9d5e3a879aeafa63f6734b9e8046733d2
Felix Freiberger <felix@fefrei.de>
2016-02-23 21:36:47 +0100

    Initial commit.

---
+++ plugin.rb
@@ -0,0 +1,33 @@
+#!/bin/env ruby
+# encoding: utf-8
+# name: pseuCo.com Onebox
+# version: 0.1
+# authors: fefrei
+
+
+Onebox = Onebox
+class Onebox::Engine::PseuCoOnebox
+    include Onebox::Engine
+
+    def self.priority
+        0
+    end
+
+    REGEX = /^https?:\/\/pseuco\.com\/\#\/edit\/remote\/([0-9a-z\-]+)$/
+
+    matches_regexp REGEX
+
+    def id
+        @url.match(REGEX)[1]
+    end
+
+    def url
+        "https://pseuco.com/#/edit/remote/#{id}"
+    end
+
+    def to_html
+        <<HTML
+        <iframe src="https://pseuco.com/#/edit/remote/#{id}"></iframe>
+HTML
+    end
+end
